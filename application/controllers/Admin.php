32d767efe132a5412e1f99a66bdf52a5d9c47a3a
Rinaldy Ramadhan <59111699+rinaldyhaha@users.noreply.github.com>
2020-01-30 13:37:37 +0700

    Update Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Admin extends CI_Controller
+class Admin extends CI_Controllerz
 {
     public function __construct()
     {
